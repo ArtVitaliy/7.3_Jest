2801c2b4837dbde82f66a98f0fdd3e2a41355bdd
ArtVitaly <0331116@mail.ru>
2023-08-15 22:48:41 +0300

    Initial commit

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,34 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ]
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]
+
+    const result = sorting.sortByName(input)
+    expect(result).toEqual(expected);
+  });
+
+  test("Unsorted books", () => {
+    const input = [
+      "Гарри Поттер",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ]
+    const expected = [
+      "Гарри Поттер",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ]
+    const result = sorting.sortByName(input)
+    expect(result).toEqual(expected);
+  })
+});
